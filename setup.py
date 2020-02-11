a5145d31d964cc13bef4d7dce63f97ac71b38e84
bherbruck <brennen.herbruck@gmail.com>
2020-02-10 23:54:29 -0500

    updated for pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
+from plend import __version__, __author__, __email__
 
-
-version_dict = {}
-exec(open('constants.py').read(), version_dict)
-VERSION = version_dict['VERSION']
-
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
 setup(name='plend',
-      version=VERSION,
+      version=__version__,
       desctiption='Python Feed Formulation',
-      auhor='Brennen Herbruck',
-      author_email='brennen.herbruck@gmail.com',
+      auhor=__author__,
+      author_email=__email__,
       license='MIT',
       url='https://github.com/bherbruck/plend',
-      install_requires=requirements,
+      install_requires=['PuLP==2.0'],
       packages=['plend', 'plend.presets'],
       python_requires='>=3.6')
