81cc274b122a7cdeb59d82bfd7151e6b283da75f
Brennen <brennen.herbruck@gmail.com>
2020-02-16 22:38:05 -0500

    updated docstrings added unit to Formula and BoundIngredient

--- plend/__init__.py
+++ plend/__init__.py
@@ -1,5 +1,2 @@
 from .core import *
-
-__version__ = '0.1.3'
-__author__ = 'Brennen Herbruck'
-__email__ = 'brennen.herbruck@gmail.com'
+from .constants import *
--- plend/core.py
+++ plend/core.py
@@ -21,10 +21,10 @@ COLUMN_HEADERS = ['library_name',
 class Nutrient():
     def __init__(self, name, code=None, unit=None):
         """Create a Nutrient
-        
+
         Args:
             name (str): name of the nutrient
-            unit (str, optional): unit of the nutrient, not yet implemented. Defaults to None.
+            unit (str, optional): unit of the nutrient. Defaults to None.
         """
         self.name = name
         self.code = code
@@ -40,7 +40,7 @@ class Nutrient():
 class IngredientNutrient():
     def __init__(self, nutrient, amount=None):
         """Nutrient with amount for use in an ingredient
-        
+
         Args:
             nutrient (Nutrient): nutrient to link
             amount (float, optional): amount of the nutrient. Defaults to None.
@@ -65,14 +65,17 @@ class IngredientNutrient():
 
 
 class Ingredient():
-    def __init__(self, name, code=None, amount=None, cost=None, nutrients=None):
+    def __init__(self, name, code=None, amount=None,
+                 cost=None, nutrients=None):
         """Create an Ingredient
         
         Args:
             name (str): name of the ingredient
-            amount (float, optional): amount of th eingredient, not yet implemented. Defaults to None.
+            amount (float, optional): amount of the ingredient. 
+                Defaults to None.
             cost (float, optional): cost of the ingredient. Defaults to None.
-            nutrients (dict, optional): formatted {[Nutrient]: amount}. Defaults to None.
+            nutrients (dict, optional): formatted {[Nutrient]: amount}. 
+                Defaults to None.
         """
         self.name = name
         self.code = code
@@ -84,7 +87,7 @@ class Ingredient():
 
     def add_nutrient(self, nutrient, amount):
         """Add a single nutrient
-        
+
         Args:
             nutrient (Nutrient): nutrient to link
             amount (float): amount of the nutrient in the ingredient
@@ -108,14 +111,17 @@ class Ingredient():
 
 
 class BoundNutrient():
-    def __init__(self, nutrient, amount=None, minimum=0, maximum=None, formula=None):
+    def __init__(self, nutrient, amount=None, minimum=0,
+                 maximum=None, formula=None):
         """Nutrient with constraints and amount
-        
+
         Args:
             nutrient (Nutrient): nutrient
             amount (float, optional): amount of the nutrient. Defaults to None.
-            minimum (float, optional): minimum amount to use in the formula. Defaults to 0.
-            maximum (float, optional): maximum amount to use in the formula. Defaults to None.
+            minimum (float, optional): minimum amount to use in the formula.
+                Defaults to 0.
+            maximum (float, optional): maximum amount to use in the formula.
+                Defaults to None.
         """
         self.nutrient = nutrient
         self.amount = amount
@@ -143,14 +149,18 @@ class BoundNutrient():
 
 
 class BoundIngredient():
-    def __init__(self, ingredient, amount=None, minimum=0, maximum=None, formula=None):
+    def __init__(self, ingredient, amount=None, minimum=0,
+                 maximum=None, formula=None):
         """Ingredient with constraints and amount
-        
+
         Args:
             ingredient (Ingredient): ingredient
-            amount (float, optional): amount of the ingredient. Defaults to None.
-            minimum (float, optional): minimum amount to use in the formula. Defaults to 0.
-            maximum (float, optional): maximum amount to use in the formula. Defaults to None.
+            amount (float, optional): amount of the ingredient.
+                Defaults to None.
+            minimum (float, optional): minimum amount to use in the formula.
+                Defaults to 0.
+            maximum (float, optional): maximum amount to use in the formula.
+                Defaults to None.
         """
         self.ingredient = ingredient
         self.amount = amount
@@ -161,7 +171,7 @@ class BoundIngredient():
     @property
     def name(self):
         return self.ingredient.name
-    
+
     @property
     def code(self):
         return self.ingredient.code
@@ -173,13 +183,18 @@ class BoundIngredient():
     @property
     def nutrients(self):
         return self.ingredient.nutrients
-    
+
     @property
     def percent(self):
         if self.formula and self.formula.batch_size and self.amount:
             return float(self.amount) / float(self.formula.batch_size)
         else:
             return None
+    
+    @property
+    def unit(self):
+        if self.formula:
+            return self.formula.unit
 
     def encode(self):
         return {'name': self.name,
@@ -193,18 +208,20 @@ class BoundIngredient():
 
 
 class Formula():
-    def __init__(self, name, code=None, batch_size=1):
+    def __init__(self, name, code=None, batch_size=1, unit=None):
         """Crate a Formula
 
         Args:
             name (str): name of the formula
             code (str): code of the formula
-            batch_size (float, optional): size of the batch, used for optimization
-                                          all ingredient constraints must apply. Defaults to 1.
+            batch_size (float, optional): size of the batch,
+                used for optimization all ingredient constraints must apply.
+                Defaults to 1.
         """
         self.name = name
         self.code = code
         self.batch_size = batch_size
+        self.unit = unit
         self.cost = 0
         self.ingredients = []
         self.nutrients = []
@@ -216,34 +233,40 @@ class Formula():
 
         Args:
             ingredient (BoundIngredient): ingredient to add
-            amount (float, optional): amount of the ingredient. Defaults to None.
-            minimum (float, optional): minimum amount to use in the formula. Defaults to 0.
-            maximum (float, optional): maximum amount to use in the formula. Defaults to None.
+            amount (float, optional): amount of the ingredient.
+                Defaults to None.
+            minimum (float, optional): minimum amount to use in the formula.
+                Defaults to 0.
+            maximum (float, optional): maximum amount to use in the formula.
+                Defaults to None.
         """
         self.ingredients.append(BoundIngredient(
             ingredient, amount, minimum, maximum, formula=self))
 
     def add_ingredients(self, ingredient_dict):
         """Add a dict of ingredients
-
+        
         Args:
             ingredient_dict (dict): formatted {Ingredient: (minimum, maximum)}
         """
         for ingredient, (minimum, maximum) in ingredient_dict.items():
-            self.add_ingredient(ingredient, minimum=minimum, maximum=maximum, formula=self)
-
+            self.add_ingredient(ingredient, minimum=minimum,
+                                maximum=maximum, formula=self)
+        
     def add_nutrient(self, nutrient, amount=None, minimum=0, maximum=None):
         """Add an nutrient with bounds to the formula
 
         Args:
             nutrient (BoundNutrient): nutrient to add
             amount (float, optional): amount of the nutrient Defaults to None.
-            minimum (float, optional): minimum amount to use in the formula. Defaults to 0.
-            maximum (float, optional): maximum amount to use in the formula. Defaults to None.
+            minimum (float, optional): minimum amount to use in the formula.
+                Defaults to 0.
+            maximum (float, optional): maximum amount to use in the formula.
+                Defaults to None.
         """
         self.nutrients.append(BoundNutrient(
             nutrient, amount, minimum, maximum, formula=self))
-        
+
     def add_nutrients(self, nutrient_dict):
         """Add a dict of nutrient
 
@@ -251,7 +274,8 @@ class Formula():
             nutrient (dict): formatted {Ingredient: (minimum, maximum)}
         """
         for nutrient, (minimum, maximum) in nutrient_dict.items():
-            self.add_nutrient(nutrient, minimum=minimum, maximum=maximum, formula=self)
+            self.add_nutrient(nutrient, minimum=minimum,
+                              maximum=maximum, formula=self)
 
     def optimize(self):
         """Optimize the formula
@@ -271,7 +295,8 @@ class Formula():
                             if i.cost])
         # total function (uses ingredient bounds from variables)
         prob += pulp.lpSum([variables[i]
-                            for i in self.ingredients]) == self.batch_size, 'total'
+                            for i in self.ingredients]) \
+                                == self.batch_size, 'total'
 
         # nutrient bounds
         for nutrient in self.nutrients:
@@ -281,16 +306,16 @@ class Formula():
                                       for i in self.ingredients
                                       for n in i.nutrients
                                       if n.name == nutrient.name])
-                prob += minimum / \
-                    self.batch_size >= nutrient.minimum, f'min_{nutrient.name}'
+                prob += minimum / self.batch_size \
+                    >= nutrient.minimum, f'min_{nutrient.name}'
             # maximum
             if nutrient.maximum:
                 maximum = pulp.lpSum([n.amount * variables[i]
                                       for i in self.ingredients
                                       for n in i.nutrients
                                       if n.name == nutrient.name])
-                prob += maximum / \
-                    self.batch_size <= nutrient.maximum, f'max_{nutrient.name}'
+                prob += maximum / self.batch_size \
+                    <= nutrient.maximum, f'max_{nutrient.name}'
 
         prob.solve()
         self.status = pulp.LpStatus[prob.status]
@@ -307,7 +332,8 @@ class Formula():
             nutrient.amount = sum([i.amount * n.amount
                                    for i in self.ingredients
                                    for n in i.nutrients
-                                   if n.name == nutrient.name]) / self.batch_size
+                                   if n.name == nutrient.name]) \
+                                       / self.batch_size
 
     def show_problem(self):
         """Prints the problem's function
@@ -343,8 +369,10 @@ class Formula():
         """Return a csv representation of the Formula
 
         Args:
-            library_name (str, optional): name of the library. Defaults to None.
-            write_header (bool, optional): whether or not to write the header row. Defaults to True.
+            library_name (str, optional): name of the library.
+                Defaults to None.
+            write_header (bool, optional): whether or not to write the header.
+                Defaults to True.
 
         Returns:
             str: csv table representation
@@ -380,21 +408,25 @@ class Formula():
         return output.getvalue()
 
     def save_csv(self, filename, library_name=None):
-        """Save the Formula as a csv, overwrites any existing file with the same name
+        """Save the Formula as a csv,
+        overwrites any existing file with the same name
 
         Args:
             filename (str): name of the file
-            library_name (str, optional): name of the library. Defaults to None.
+            library_name (str, optional): name of the library.
+                Defaults to None.
         """
         with open(filename, 'w', newline='') as file:
-            file.write(self.to_csv(library_name=library_name, write_header=True))
+            file.write(self.to_csv(
+                library_name=library_name, write_header=True))
 
 
 class FormulaLibrary():
     """A library of nutrients, ingredients, and formulas
     """
 
-    def __init__(self, name, units=None, nutrients=None, ingredients=None, formulas=None):
+    def __init__(self, name, units=None, nutrients=None,
+                 ingredients=None, formulas=None):
         """[summary]
 
         Args:
@@ -452,11 +484,13 @@ class FormulaLibrary():
         writer.writerow(COLUMN_HEADERS)
         csv_string += output.getvalue()
         for formula in self.formulas:
-            csv_string += formula.to_csv(library_name=self.name, write_header=False)
+            csv_string += formula.to_csv(library_name=self.name,
+                                         write_header=False)
         return csv_string
 
     def save_csv(self, filename):
-        """Save the FormulaLibrary as a csv, overwrites any existing file with the same name
+        """Save the FormulaLibrary as a csv,
+        overwrites any existing file with the same name
 
         Args:
             filename (str): name of the file
