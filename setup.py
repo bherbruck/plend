156e077fb1c18f010347fccf2d5b633fc95038bc
bherbruck <brennen.herbruck@gmail.com>
2020-03-11 18:10:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from plend import constants
 
 setup(name='plend',
       version=constants.VERSION,
-      desctiption='Least Cost Formulation with Python',
-      auhor=constants.AUTHOR,
+      description='Least Cost Formulation with Python',
+      author=constants.AUTHOR,
       author_email=constants.EMAIL,
       license='MIT',
       url='https://github.com/bherbruck/plend',
