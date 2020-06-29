d8d51ba76c0d8c74bf8fc9ecdef2f1bc29879148
bherbruck <brennen.herbruck@gmail.com>
2020-06-29 08:30:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
 
-setup(name='plend',
-      version='0.1.4',
-      description='Least Cost Formulation with Python',
-      author='Brennen Herbruck',
-      author_email='brennen.herbruck@gmail.com',
-      license='MIT',
-      url='https://github.com/bherbruck/plend',
-      install_requires=['PuLP==2.0'],
-      packages=['plend', 'plend.presets'],
-      python_requires='>=3.6')
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='plend',
+    version='0.1.4',
+    author='Brennen Herbruck',
+    author_email='brennen.hrbruck@gmail.com',
+    description='Least Cost Formulation with Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bherbruck/plend',
+    packages=['plend', 'plend.presets'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
