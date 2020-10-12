20ee67043584e459cf0fbce9c515840293214e93
bherbruck <brennen.herbruck@gmail.com>
2020-10-12 15:52:34 -0400

    remove random functions

--- plend/utils.py
+++ plend/utils.py
@@ -1,23 +1,4 @@
-import csv
-
 from typing import List, Dict, Any
-
-
-def read_csv(csv_file: str) -> List[List[Any]]:
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        return [row for row in reader]
-
-
-def csv_to_dict(csv_file: str,
-                headers: List[str] = None) -> List[Dict[str, Any]]:
-    csv_rows = read_csv(csv_file)
-    if not headers:
-        headers = csv_rows.pop(0)
-    csv_dict = [{header: row[h_idx]
-                 for h_idx, header in enumerate(headers)}
-                for row in csv_rows]
-    return(csv_dict)
 
 
 def clean_name(name, replacement: str = '_') -> str:
@@ -36,55 +17,3 @@ def clean_name(name, replacement: str = '_') -> str:
     if not cleaned_name.isidentifier():
         raise ValueError()
     return cleaned_name
-
-
-def csv_to_nutrients_py(csv_file, output_file=None, headers=None, nut_pre=''):
-    nutrient_dict = csv_to_dict(csv_file, headers)
-    lines = [f"{nut_pre}{clean_name(row['Name'])} = Nutrient('{row['Name']}', code='{row['Code']}', unit='{row['Unit']}')"
-             for row in nutrient_dict]
-    if output_file:
-        lines.insert(0, 'from plend import Nutrient')
-    output = "\n".join(lines)
-    if output_file:
-        with open(output_file, 'w') as file:
-            file.write(output)
-    return output
-
-
-def csv_to_ingredients_py(csv_file, output_file=None, headers=None, ing_pre=''):
-    nutrient_dict = csv_to_dict(csv_file, headers)
-    lines = [f"{ing_pre}{clean_name(row['Name'])} = Ingredient('{row['Name']}', code='{row['Code']}', cost={float(row['Cost'])})"
-             for row in nutrient_dict]
-    if output_file:
-        lines.insert(0, 'from plend import Ingredient')
-    output = "\n".join(lines)
-    if output_file:
-        with open(output_file, 'w') as file:
-            file.write(output)
-    return output
-
-
-def csv_to_ing_nut_py(csv_file, output_file=None, headers=None, ing_pre='', nut_pre=''):
-    nutrient_dict = csv_to_dict(csv_file, headers)
-    lines = [f"{ing_pre}{clean_name(row['Name'])}.add_nutrient({nut_pre}{clean_name(row['Nutrient'])}, amount={float(row['Amount'])})"
-             for row in nutrient_dict]
-    if output_file:
-        lines.insert(0, 'from plend import Nutrient, Ingredient')
-    output = "\n".join(lines)
-    if output_file:
-        with open(output_file, 'w') as file:
-            file.write(output)
-    return output
-
-
-def write_text(filename, text):
-    with open(filename, 'w') as file:
-        file.write(text)
-
-
-def generate_file(filename, *lines):
-    text = ''
-    for line in lines:
-        text += line + '\n'
-    if len(text) > 0:
-        write_text(filename, text)
