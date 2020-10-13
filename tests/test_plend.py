98801b74af20ef538e5da9673a67cee22e05914a
bherbruck <brennen.herbruck@gmail.com>
2020-10-13 08:59:36 -0400

    Update test_plend.py

--- tests/test_plend.py
+++ tests/test_plend.py
@@ -1,87 +1,94 @@
-import unittest
-
-from plend import Formula, FormulaLibrary, Ingredient, Nutrient
+from plend import Item, Formula, FormulaLibrary, Ingredient, Nutrient
 from plend.presets.poultry import *
+from plend.utils import clean_name
 
 
-class TestFormulaLibrary(unittest.TestCase):
+test_data = {
+    'nutrients': [
+        {'name': 'moisture'},
+        {'name': 'carbon', 'code': 'cbn'}
+    ],
+    'ingredients': [
+        {'name': 'water', 'nutrients': [
+            {'name': 'moisture', 'amount': 100},
+        ]},
+        {'name': 'dirt', 'code': '123456', 'nutrients': [
+            {'name': 'moisture', 'amount': 0.75},
+            {'name': 'carbon', 'amount': 0.15}
+        ]},
+    ],
+    'formulas': [
+        {
+            'name': 'mud pie',
+            'nutrients': [
 
-    def setUp(self):
-        self.flib_csv = """library_name,formula_name,formula_code,formula_cost,formula_status,item_type,item_name,item_code,item_amount,item_minimum,item_maximum
-Poultry,Starter,starter,0,Unsolved,ingredient,Corn,corn,,1,
-Poultry,Starter,starter,0,Unsolved,ingredient,Soybean Meal,soybean_meal,,0,
-Poultry,Starter,starter,0,Unsolved,ingredient,Oil,oil,,0,5
-Poultry,Starter,starter,0,Unsolved,ingredient,Limestone,limestone,,0,
-Poultry,Starter,starter,0,Unsolved,ingredient,Meat Meal,meat_meal,,0,10
-Poultry,Starter,starter,0,Unsolved,nutrient,Energy,energy,,3000,
-Poultry,Starter,starter,0,Unsolved,nutrient,Protein,protein,,20,
-Poultry,Starter,starter,0,Unsolved,nutrient,Fiber,fiber,,0,
-Poultry,Starter,starter,0,Unsolved,nutrient,Calcium,calcium,,4,5
-Poultry,Finisher,finisher,0,Unsolved,ingredient,Corn,corn,,1,
-Poultry,Finisher,finisher,0,Unsolved,ingredient,Soybean Meal,soybean_meal,,0,
-Poultry,Finisher,finisher,0,Unsolved,ingredient,Oil,oil,,0,5
-Poultry,Finisher,finisher,0,Unsolved,ingredient,Limestone,limestone,,0,
-Poultry,Finisher,finisher,0,Unsolved,ingredient,Meat Meal,meat_meal,,0,10
-Poultry,Finisher,finisher,0,Unsolved,nutrient,Energy,energy,,3000,
-Poultry,Finisher,finisher,0,Unsolved,nutrient,Protein,protein,,20,
-Poultry,Finisher,finisher,0,Unsolved,nutrient,Fiber,fiber,,0,
-Poultry,Finisher,finisher,0,Unsolved,nutrient,Calcium,calcium,,4,5"""
-
-        self.starter_csv = """library_name,formula_name,formula_code,formula_cost,formula_status,item_type,item_name,item_code,item_amount,item_minimum,item_maximum
-default,Starter,starter,0,Unsolved,ingredient,Corn,corn,,1,
-default,Starter,starter,0,Unsolved,ingredient,Soybean Meal,soybean_meal,,0,
-default,Starter,starter,0,Unsolved,ingredient,Oil,oil,,0,5
-default,Starter,starter,0,Unsolved,ingredient,Limestone,limestone,,0,
-default,Starter,starter,0,Unsolved,ingredient,Meat Meal,meat_meal,,0,10
-default,Starter,starter,0,Unsolved,nutrient,Energy,energy,,3000,
-default,Starter,starter,0,Unsolved,nutrient,Protein,protein,,20,
-default,Starter,starter,0,Unsolved,nutrient,Fiber,fiber,,0,
-default,Starter,starter,0,Unsolved,nutrient,Calcium,calcium,,4,5"""
-
-        self.starter = Formula(name='Starter', batch_size=100)
-        self.starter.add_ingredient(corn, minimum=1)
-        self.starter.add_ingredient(soybean_meal)
-        self.starter.add_ingredient(oil, maximum=5)
-        self.starter.add_ingredient(limestone)
-        self.starter.add_ingredient(meat_meal, maximum=10)
-        self.starter.add_nutrient(energy, minimum=3000)
-        self.starter.add_nutrient(protein, minimum=20)
-        self.starter.add_nutrient(fiber)
-        self.starter.add_nutrient(calcium, minimum=4, maximum=5)
-
-        self.finisher = Formula(name='Finisher', batch_size=100)
-        self.finisher.add_ingredient(corn, minimum=1)
-        self.finisher.add_ingredient(soybean_meal)
-        self.finisher.add_ingredient(oil, maximum=5)
-        self.finisher.add_ingredient(limestone)
-        self.finisher.add_ingredient(meat_meal, maximum=10)
-        self.finisher.add_nutrient(energy, minimum=3000)
-        self.finisher.add_nutrient(protein, minimum=20)
-        self.finisher.add_nutrient(fiber)
-        self.finisher.add_nutrient(calcium, minimum=4, maximum=5)
-
-        self.flib = FormulaLibrary(name='Poultry')
-        self.flib.add_formulas([self.starter, self.finisher])
-
-        self.starter_out = self.starter.to_csv()
-        self.finisher_out = self.finisher.to_csv()
-        self.flib_out = self.flib.to_csv()
-
-    def test_FormulaLibrary_to_csv(self):
-        self.compare_csv(self.flib_out.splitlines(),
-                         self.flib_csv.splitlines())
-
-    def test_Formula_to_csv(self):
-        self.compare_csv(self.starter_out.splitlines(),
-                         self.starter_csv.splitlines())
-    
-    def test_Formula_methods(self):
-        self.starter.optimize()
-
-    def compare_csv(self, csv1, csv2):
-        for line in [(x, y) for x, y in zip(csv1, csv2)]:
-            self.assertEqual(line[0], line[1])
+            ],
+            'ingredients': []
+        }
+    ]
+}
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_Item_init():
+    item = Item('item 1')
+    assert item.name == 'item 1'
+    assert item.code == 'item_1'
+
+
+def test_Nutrient_init():
+    for i in test_data['nutrients']:
+        name = i.get('name')
+        code = i.get('code')
+        item = Nutrient(name, code)
+        assert item.name == name
+        assert item.code == code or clean_name(name)
+
+
+def test_Ingredient():
+    for i in test_data['ingredients']:
+        name = i.get('name')
+        code = i.get('code')
+        item = Ingredient(name, code)
+        assert item.name == name
+        assert item.code == code or clean_name(name)
+
+
+def test_solve_formula():
+    carbon = Nutrient('Carbon')
+    moisture = Nutrient('Moisture', code='wetness')
+
+    dirt = Ingredient('Dirt', cost=10, nutrients={
+        carbon: 0.85,
+        moisture: 0.1
+    })
+    water = Ingredient('Water', cost=15, nutrients={
+        moisture: 1
+    })
+
+    mud = Formula('Mud', batch_size=2000)
+    mud.add_ingredients({
+        dirt:  (0, None),
+        water: (0, None)
+    })
+    mud.add_nutrients({
+        carbon:   (0.5, None),
+        moisture: (0.2, None)
+    })
+
+    mud.optimize()
+
+    print(mud.problem)
+
+    print(mud.status)
+    for ingredient in mud.ingredients:
+        assert ingredient.amount >= ingredient.minimum
+        assert ingredient.amount <= ingredient.maximum
+        print(ingredient.maximum)
+
+    for nutrient in mud.nutrients:
+        total_nutrient = sum([i.amount * n.amount / mud.batch_size
+                              for i in mud.ingredients
+                              for n in i.nutrients
+                              if n.name == nutrient.name])
+        assert total_nutrient >= nutrient.minimum
+        assert total_nutrient <= (nutrient.maximum or total_nutrient)
